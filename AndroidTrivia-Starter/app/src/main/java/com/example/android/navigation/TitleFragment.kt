9e54b424f3302270294a84d15b4af2f8d034b21a
Piotr Styła <31997939+PiotrStyla@users.noreply.github.com>
2020-07-24 12:00:29 +0200

    Menu + navigation

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,14 +1,13 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -43,9 +42,20 @@ class TitleFragment : Fragment() {
             binding.playButton.setOnClickListener { view : View ->
                 view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
             }
+            setHasOptionsMenu(true)
         }
         return binding.root
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+                view!!.findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
     companion object {
